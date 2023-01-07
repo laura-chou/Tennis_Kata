3cd33fcdedfd88ab20a7963523b13693cc4be215
Yu-Lun <chou522128@gmail.com>
2023-01-07 22:26:39 +0800

    [Refactor] add SetUp and ScoreResultShouldReturn function

--- Tennis/Tennis/TennisGameTest.cs
+++ Tennis/Tennis/TennisGameTest.cs
@@ -6,21 +6,31 @@ namespace Tennis
     [TestFixture]
     public class TennisGameTest
     {
+        private TennisGame _tennis;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennis = new TennisGame();
+        }
 
         [Test]
         public void A01_Love_All()
         {
-            var tennis = new TennisGame();
-            var actual = tennis.ScoreResult();
-            actual.Should().Be("Love All");
+            ScoreResultShouldReturn("Love All");
         }
+
         [Test]
         public void A02_Fifteen_Love()
         {
-            var tennis = new TennisGame();
-            tennis.GivePlayer1Score();
-            var actual = tennis.ScoreResult();
-            actual.Should().Be("Fifteen Love");
+            _tennis.GivePlayer1Score();
+            ScoreResultShouldReturn("Fifteen Love");
+        }
+
+        private void ScoreResultShouldReturn(string expected)
+        {
+            var actual = _tennis.ScoreResult();
+            actual.Should().Be(expected);
         }
     }
 }
