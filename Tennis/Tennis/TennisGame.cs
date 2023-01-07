0c161c7636b770c1e682f72ef049f0a2e8579130
Yu-Lun <chou522128@gmail.com>
2023-01-07 22:05:07 +0800

    [Red] A01_Love_All：add new test case

---
+++ Tennis/Tennis/TennisGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tennis
+{
+    public class TennisGame
+    {
+        public string ScoreResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tennis/Tennis/TennisGameTest.cs
+++ Tennis/Tennis/TennisGameTest.cs
@@ -4,8 +4,14 @@ using NUnit.Framework;
 namespace Tennis
 {
     [TestFixture]
-    public class GameTest
+    public class TennisGameTest
     {
-
+        [Test]
+        public void A01_Love_All()
+        {
+            var tennis = new TennisGame();
+            var actual = tennis.ScoreResult();
+            actual.Should().Be("Love All");
+        }
     }
 }
