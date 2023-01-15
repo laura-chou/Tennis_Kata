65f8ffece1c7e6f9cafc96b0fe5da6c73b4db92a
Yu-Lun <chou522128@gmail.com>
2023-01-15 21:53:53 +0800

    [Green] A13_Player2_Advantage：compare players score and get winner name

--- Tennis/Tennis/TennisGame.cs
+++ Tennis/Tennis/TennisGame.cs
@@ -35,7 +35,8 @@ namespace Tennis
 
             if (_player1Score >= 3 && Math.Abs(_player1Score - _player2Score) == 1)
             {
-                return $"{_player1Name} Advantage";
+                var winnerName = _player1Score > _player2Score ? _player1Name : "Player B";
+                return $"{winnerName} Advantage";
             }
 
             return $"{_scoreLookLike[_player1Score]} {_scoreLookLike[_player2Score]}";
