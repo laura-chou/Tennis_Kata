29632badad333c8f5d149c97216362b53c993331
Yu-Lun <chou522128@gmail.com>
2023-01-15 21:40:51 +0800

    [Refactor] refactor GameResult function

--- Tennis/Tennis/TennisGame.cs
+++ Tennis/Tennis/TennisGame.cs
@@ -16,16 +16,16 @@ namespace Tennis
         };
         public string GameResult()
         {
-            if (_player1Score != _player2Score)
+            if (_player1Score == _player2Score)
             {
-                return $"{_scoreLookLike[_player1Score]} {_scoreLookLike[_player2Score]}";
+                if (_player1Score >= 3 && _player2Score >= 3)
+                {
+                    return "Deuce";
+                }
+                return $"{_scoreLookLike[_player1Score]} All";
             }
 
-            if (_player1Score >= 3)
-            {
-                return "Deuce";
-            }
-            return $"{_scoreLookLike[_player1Score]} All";
+            return $"{_scoreLookLike[_player1Score]} {_scoreLookLike[_player2Score]}";
         }
 
         public void GivePlayer1ScoreTimes(int times)
