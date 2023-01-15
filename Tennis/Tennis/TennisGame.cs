1ca1093cbea75fbb8f81c171b25b11d1f00065a1
Yu-Lun <chou522128@gmail.com>
2023-01-15 22:18:55 +0800

    [Refactor] refactor GameResult function

--- Tennis/Tennis/TennisGame.cs
+++ Tennis/Tennis/TennisGame.cs
@@ -25,16 +25,16 @@ namespace Tennis
 
         public string GameResult()
         {
-            if (_player1Score == _player2Score)
+            if (IsSameScore())
             {
-                if (_player1Score >= 3 && _player2Score >= 3)
+                if (_player1Score >= 3)
                 {
                     return "Deuce";
                 }
                 return $"{_scoreLookLike[_player1Score]} All";
             }
 
-            if (_player1Score >= 3 && _player2Score >= 3)
+            if (IsReadyGamePoint())
             {
                 var winnerName = _player1Score > _player2Score ? _player1Name : _player2Name;
                 var scoreGap = Math.Abs(_player1Score - _player2Score);
@@ -47,6 +47,16 @@ namespace Tennis
             }
 
             return $"{_scoreLookLike[_player1Score]} {_scoreLookLike[_player2Score]}";
+        }
+
+        private bool IsReadyGamePoint()
+        {
+            return _player1Score >= 3 && _player2Score >= 3;
+        }
+
+        private bool IsSameScore()
+        {
+            return _player1Score == _player2Score;
         }
 
         public void GivePlayer1ScoreTimes(int times)
